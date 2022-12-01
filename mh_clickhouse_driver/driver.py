597028f7696305edb85c17e4a919284a174a749b
Vladimir Trifonov <3fonov@gmail.com>
2022-12-01 13:29:13 +0300

    Initial commit

--- mh_clickhouse_driver/driver.py
+++ mh_clickhouse_driver/driver.py
@@ -1,2 +1,69 @@
+import logging
+import os
+
+import numpy as np
+from clickhouse_driver import Client
+from pandas import DataFrame
+
+
 class MHClickhouseDriver():
-    pass
+    def __init__(self):
+        self.client = Client(host=os.getenv('CLICKHOUSE_HOST'),
+                             port=os.getenv('CLICKHOUSE_PORT'),
+                             database=os.getenv('CLICKHOUSE_DATABASE'),
+                             user=os.getenv('CLICKHOUSE_USER'),
+                             password=os.getenv('CLICKHOUSE_PASSWORD'),
+                             secure='True',
+                             settings={'use_numpy': True})
+        self.logger = logging.getLogger("MHClickhouseDriver")
+
+    FIELD_TYPES = {np.dtype('int64'): 'Int64',
+                   np.dtype('<M8[ns]'): 'DateTime',
+                   np.dtype('O'): 'String',
+                   np.dtype('bool'): 'UInt8',
+                   np.dtype('float64'): 'Float'}
+
+    def get_field(self, name, type):
+        field_type = self.FIELD_TYPES.get(type, 'String')
+        return f"{name} {field_type}"
+
+    def get_table_fields(self, df: DataFrame):
+        fields = [self.get_field(name, type)
+                  for name, type in df.dtypes.items()]
+        return fields
+
+    def create_table_if_not_exists(self, df: DataFrame, table_name: str,
+                                   order_by: str, partition_by: str = None):
+        nl = ',\n'
+        if partition_by:
+            order_by = partition_by
+            partition_by_sql = f'PARTITION BY {partition_by}'
+        else:
+            partition_by_sql = ''
+
+        fields = self.get_table_fields(df)
+
+        query = f'''CREATE TABLE IF NOT EXISTS
+        {self.client.connection.database}.{table_name}
+        (
+        {nl.join(fields)}
+        )
+        ENGINE = MergeTree()
+        {partition_by_sql}
+        ORDER BY {order_by};'''
+        self.logger.info("Creating table\n%s" % query)
+        self.client.execute(query)
+
+    def upload_df(self, df: DataFrame, destination_table: str,
+                  order_by: str, partition_by: str = None):
+
+        self.create_table_if_not_exists(
+            df, destination_table, order_by, partition_by)
+
+        db = self.client.connection.database
+        count = self.client.insert_dataframe(
+            f'INSERT INTO {db}.{destination_table} VALUES',
+            df)
+
+        self.logger.info("Inserted %s records" % count)
+        return count
--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,62 @@
+import glob
+import os
 import unittest
+
+import dotenv
+from clickhouse_driver import Client
+from generate_fake_df import generate_fake_dataframe
+from pandas import read_csv
+
+from mh_clickhouse_driver.driver import MHClickhouseDriver
+
+dotenv.load_dotenv()
 
 
 class TestMHClickhouseDriver(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = Client(host=os.getenv('CLICKHOUSE_HOST'),
+                             port=os.getenv('CLICKHOUSE_PORT'),
+                             database=os.getenv('CLICKHOUSE_DATABASE'),
+                             user=os.getenv('CLICKHOUSE_USER'),
+                             password=os.getenv('CLICKHOUSE_PASSWORD'),
+                             secure='True',
+                             settings={'use_numpy': True})
+        db = self.client.connection.database
+        tables = self.client.execute(f'SHOW TABLES IN {db};')
+        for table in tables[0]:
+            query = (f'DROP TABLE IF EXISTS {db}.{table};')
+            self.client.execute(query)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+
+        self.client.disconnect()
+        return super().tearDown()
 
     def test_dataframe_upload(self):
-        pass
+        df = generate_fake_dataframe(size=100, cols="cifcdtb")
+        table_name = 'test_dataframe_upload'
+
+        driver = MHClickhouseDriver()
+        count = driver.upload_df(df, table_name, 'column_5_datetime')
+
+        self.assertEqual(count, 100)
+
+        db = self.client.connection.database
+        query = f"SELECT COUNT(*) FROM {db}.{table_name};"
+        result = self.client.execute(query)
+        self.assertEqual(result[0][0], 100)
+
+    def test_testdata_uploads(self):
+        driver = MHClickhouseDriver()
+        for filepath in glob.glob(os.path.join('tests/test_data', '*.csv')):
+            df = read_csv(filepath)
+            print(df.head())
+            names = os.path.basename(filepath).split('.')[0].split('_')
+
+            count = driver.upload_df(df, names[0], names[1])
+
+            self.assertEqual(len(df), count)
 
 
 if __name__ == "__main__":
