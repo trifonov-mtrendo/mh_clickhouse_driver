7c2ae22c45b7f2edb754fbecf602e021d6fc83fe
Vladimir Trifonov <3fonov@gmail.com>
2022-12-01 11:58:55 +0300

    first commit

---
+++ mh_clickhouse_driver/driver.py
@@ -0,0 +1,2 @@
+class MHClickhouseDriver():
+    pass
---
+++ tests/tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestMHClickhouseDriver(unittest.TestCase):
+
+    def test_dataframe_upload(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
